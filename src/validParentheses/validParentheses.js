661f3e553334d3755c6c59a70be67fd3ed82e658
sagar <sagar@fabric.inc>
2020-03-15 22:05:23 +0530

    ADD: Valid Parentheses

---
+++ src/validParentheses/validParentheses.js
@@ -0,0 +1,32 @@
+const isValid = (expression) => {
+  if (!expression) return true
+  if (expression.length % 2 !== 0) return false
+
+  let rightExp = [')', '}', ']']
+
+  if (rightExp.includes(expression[0])) return false
+
+  let stack = []
+  let leftExp = ['(', '{', '[']
+
+  for (let i = 0; i < expression.length; i++) {
+    if (leftExp.includes(expression[i])) {
+      stack.push(expression[i])
+      continue
+    }
+
+    if (rightExp.includes(expression[i])) {
+      if (stack.length === 0) return false
+
+      const lastItem = stack[stack.length - 1]
+
+      if (leftExp.indexOf(lastItem) === rightExp.indexOf(expression[i])) {
+        stack.pop()
+      } else {
+        stack.push(expression[i])
+      }
+    }
+  }
+
+  return !stack.length
+}
